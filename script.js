ceaf9dff0b6c1559c309daf0a4c7963b6e3f899a
Gul Ali <110970153+imgulali@users.noreply.github.com>
2024-08-07 20:39:33 +0500

    Resolved: Undefined

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 function generatePassword() {
-    const length = document.getElementById('length').value;
-    const includeLowercase = document.getElementById('include-lowercase').checked;
-    const includeUppercase = document.getElementById('include-uppercase').checked;
-    const includeNumbers = document.getElementById('include-numbers').checked;
-    const includeChars = document.getElementById('include-chars').checked;
-   
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numberChars = '0123456789';
-    const specialChars = '!@#$%^&*(){}[]?';
-    
-    let charPool = '';
-    if (includeLowercase) charPool += lowercaseChars;
-    if (includeUppercase) charPool += uppercaseChars;
-    if (includeNumbers) charPool += numberChars;
-    if (includeChars) charPool += specialChars;
+  const length = document.getElementById("length").value;
+  const includeLowercase = document.getElementById("include-lowercase").checked;
+  const includeUppercase = document.getElementById("include-uppercase").checked;
+  const includeNumbers = document.getElementById("include-numbers").checked;
+  const includeChars = document.getElementById("include-chars").checked;
 
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const specialChars = "!@#$%^&*(){}[]?";
 
-    let password = '';
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charPool.length);
-        password += charPool[randomIndex];
-    }
+  let checked = 0;
+  let charPool = "";
+  if (includeLowercase) {
+    charPool += lowercaseChars;
+    checked++;
+  }
+  if (includeUppercase) {
+    charPool += uppercaseChars;
+    checked++;
+  }
+  if (includeNumbers) {
+    charPool += numberChars;
+    checked++;
+  }
+  if (includeChars) {
+    charPool += specialChars;
+    checked++;
+  }
 
-    document.getElementById('password').value = password;
+  if(checked == 0){
+    document.getElementById("password").value = "Select Atleast One Option";
+    return;
+  }
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charPool.length);
+    password += charPool[randomIndex];
+  }
+
+  document.getElementById("password").value = password;
 }
