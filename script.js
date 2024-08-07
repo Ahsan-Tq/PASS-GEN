bb8658b306cde05059c0f745b738e37db54062a2
Gul Ali <110970153+imgulali@users.noreply.github.com>
2024-08-07 20:23:30 +0500

    Special Chars

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ function generatePassword() {
     const includeLowercase = document.getElementById('include-lowercase').checked;
     const includeUppercase = document.getElementById('include-uppercase').checked;
     const includeNumbers = document.getElementById('include-numbers').checked;
+    const includeChars = document.getElementById('include-chars').checked;
    
     const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
     const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const numberChars = '0123456789';
+    const specialChars = '!@#$%^&*(){}[]?';
     
     let charPool = '';
     if (includeLowercase) charPool += lowercaseChars;
     if (includeUppercase) charPool += uppercaseChars;
     if (includeNumbers) charPool += numberChars;
+    if (includeChars) charPool += specialChars;
 
 
     let password = '';
